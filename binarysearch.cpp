d6002f144cc2a65fca99412913d3a522e128dc8a
AnggaWdnt <widiangga94@gmail.com>
2026-04-17 15:15:49 +0700

    Menambahkan input elemen dan inisialisasi batas

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -75,5 +75,11 @@ void binarySearch()
         cout << "\n========================================\n";
         cout << "        Pencarian Binary Search\n";
         cout << "========================================\n";
+
+        cout << "Masukkan elemen yang ingin dicari: ";
+        cin >> x;
+
+        int low = 0;
+        int high = nPanjang - 1;
     }
 }
