f721b5cf1fb99fc2b381562530536928d56ac0c8
AnggaWdnt <widiangga94@gmail.com>
2026-04-17 14:48:21 +0700

    Membuat fungsi input untuk menerima jumlah elemen

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -5,3 +5,11 @@ int elemen[10];
 int nPanjang;
 int x;
 
+void input()
+{
+    while(true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (makasimal 10) : ";
+        cin >> nPanjang;
+    }
+}
