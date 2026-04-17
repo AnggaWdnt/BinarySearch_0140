9cb6d063df83c610dce3bf9f0f73a8136bc99d9f
AnggaWdnt <widiangga94@gmail.com>
2026-04-17 14:41:21 +0700

    first commit

---
+++ binarysearch.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int elemen[10];
+int nPanjang;
+int x;
+
+void input()
+{
+    while(true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (makasimal 10) : ";
+        cin >> nPanjang;
+    }
+}
