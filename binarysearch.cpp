767bc4e65b43c1aecaf80188bfc5acce6b08bdca
AnggaWdnt <widiangga94@gmail.com>
2026-04-17 14:42:54 +0700

    Menambahkan deklarasi variabel

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -5,11 +5,3 @@ int elemen[10];
 int nPanjang;
 int x;
 
-void input()
-{
-    while(true)
-    {
-        cout << "Masukkan banyaknya elemen pada array (makasimal 10) : ";
-        cin >> nPanjang;
-    }
-}
